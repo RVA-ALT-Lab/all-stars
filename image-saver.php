aff930f1c0884929483b83657b4d9622a716f5e9
Tom Woodward <twwoodward@me.com>
2020-04-30 08:59:17 -0400

    remove 2020, clean img title

--- image-saver.php
+++ image-saver.php
@@ -7,14 +7,16 @@
     $data = json_decode($json);
     $name = $data->name;
     echo $name;
+    $clean_name = (explode( ' - ', $name)[0]);//nice clean image name so people don't go insane
+    $clean_name = str_replace(' ', '', $clean_name);
+    $clean_name = strtolower($clean_name);
     $file = 'data:image/jpeg;base64,' . strval($data->bytes);
-    var_dump($file);
-	//$img = 'data:image/png;base64,gAAAQ8AAAC6CAMAAACHgTh+AA=';
-
+    var_dump($clean_name);
+    //thanks to https://stackoverflow.com/questions/11511511/how-to-save-a-png-image-server-side-from-a-base64-data-string
 	list($type, $file) = explode(';', $file);
 	list(, $file)      = explode(',', $file);
 	$file = base64_decode($file);
 
-	file_put_contents('imgs/'.$name.'.jpg', $file);
+	file_put_contents('work/' . $clean_name . '.jpg', $file);
 
 ;?>
